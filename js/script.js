a4acf742dd80d44c6fe1d96311a3542e34744668
Adamgast <92505201+Adamgast@users.noreply.github.com>
2021-12-05 19:24:35 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -66,3 +66,100 @@ new Swiper('.material__slider',{
     loop:true,
 });
 //=================================================================================================================================================================================
+
+// POPUP====================================================================================================================================================================================
+const popupLinks = document.querySelectorAll('.popup-link');
+const lockPadding = document.querySelectorAll('.lock-padding');
+const body = document.querySelector('body');
+let unlock = true;
+const timeout = 300;
+
+if (popupLinks.length>0) {
+	for(let index = 0; index<popupLinks.length; index++){
+		const popupLink = popupLinks[index];
+		popupLink.addEventListener("click", function(e){
+			const popupName = popupLink.getAttribute('href').replace('#', '');
+			const curentPopup = document.getElementById(popupName);
+			popupOpen(curentPopup);
+			e.preventDefault();
+		});
+	}
+}
+
+ //Закрывающий объект
+const popupCloseIcon = document.querySelectorAll('.close-popup');
+if (popupCloseIcon.length>0) {
+	for(let index = 0; index < popupCloseIcon.length; index ++){
+		const el = popupCloseIcon[index];
+		el.addEventListener('click', function(e){
+			popupClose(el.closest('.popup,.popup-2'));
+			e.preventDefault();
+		});
+	}
+}
+
+ //Функция открытия POPUP
+ function popupOpen(curentPopup){
+ 	if (curentPopup && unlock) {
+ 		const popupActive = document.querySelector('.popup._open');
+ 		if (popupActive) {
+ 			popupClose(popupActive, false);
+ 		} else{
+ 			bodyLock();
+ 		}
+ 		curentPopup.classList.add('_open');
+ 		curentPopup.addEventListener("click", function(e){
+ 			if (!e.target.closest('.popup__content')) {
+ 				popupClose(e.target.closest('.popup,.popup-2'));
+ 			}
+ 		});
+ 	}
+ }
+
+
+function popupClose(popupActive, doUnlock = true){
+	if (unlock) {
+		popupActive.classList.remove('_open');
+		if (doUnlock) {
+			bodyUnLock();
+		}
+	}
+}
+
+
+ function bodyLock(){
+ 	const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+
+ 	if (lockPadding.length>0) {
+	 	for(let index = 0; index < lockPadding.length; index++){
+	 		const el = lockPadding[index];
+	 		el.style.paddingRight = lockPaddingValue;
+	 	}
+	}
+ 	body.style.paddingRight = lockPaddingValue;
+ 	body.classList.add('_lock');
+
+ 	unlock = false;
+ 	setTimeout(function(){
+ 		unlock = true;
+ 	}, timeout);
+ }
+
+ function bodyUnLock(){
+ 	setTimeout(function(){
+ 		if (lockPadding.length>0) {
+	 		for(let index = 0; index < lockPadding.length; index++){
+	 			const el = lockPadding[index];
+	 			el.style.paddingRight = '0px';
+	 		}
+	 	}	
+ 		body.style.paddingRight = '0px';
+ 		body.classList.remove('_lock');
+ 	}, timeout);
+
+ 	unlock = false;
+ 	setTimeout(function(){
+ 		unlock = true;
+ 	}, timeout);
+ }
+//======================================================================================================================================================================================================
